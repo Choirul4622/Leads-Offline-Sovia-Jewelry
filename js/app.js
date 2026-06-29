40bbfed4fa6cafb92b9e41105bc87a2b3ec4a4e7
Choirul4622 <44554547+Choirul4622@users.noreply.github.com>
2026-06-29 17:19:57 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -192,8 +192,10 @@ document.addEventListener('DOMContentLoaded', async () => {
             // Update panels
             elements.panels.forEach(panel => {
                 panel.classList.remove('active');
+                panel.classList.add('hidden');
                 if (panel.id === `panel-${targetId}`) {
                     panel.classList.add('active');
+                    panel.classList.remove('hidden');
                 }
             });
 
